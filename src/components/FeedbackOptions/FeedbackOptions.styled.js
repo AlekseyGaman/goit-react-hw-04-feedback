b4412150185d9deb962eab447324b269fa2481ac
AlekseyGaman <4117988@gmail.com>
2022-12-04 22:44:58 +0200

    changes-2

--- src/components/FeedbackOptions/FeedbackOptions.styled.js
+++ src/components/FeedbackOptions/FeedbackOptions.styled.js
@@ -12,17 +12,17 @@ export const FeedbackButton = styled.button`
   padding: 10px;
   border: none;
   box-shadow: 0px 10px 13px -7px #000000, 0px 0px 9px 5px rgba(0, 0, 0, 0);
-  &:hover {
+  /* &:hover {
     transform: scale(1.2);
     background-color: ${prop => {
-      switch (prop.name) {
-        case 'good':
-          return '#82fa4d';
-        case 'neutral':
-          return '#4d7cfa';
-        case 'bad':
-          return '#fa4d4d ';
-      }
-    }};
-  }
+    switch (prop.name) {
+      case 'good':
+        return '#82fa4d';
+      case 'neutral':
+        return '#4d7cfa';
+      case 'bad':
+        return '#fa4d4d ';
+    }
+  }}; */
+  /* } */
 `;
